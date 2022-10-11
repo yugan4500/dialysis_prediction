dd6467fcbd27f57e5a3e5bc7d98e3fd8b809c7c7
yugan4500 <110961225+yugan4500@users.noreply.github.com>
2022-10-11 13:55:07 +0530

    Update main.py

--- main.py
+++ main.py
@@ -33,39 +33,39 @@ st.subheader('For CKD patient in stage G4 or at the end of stage 3(G3b)，how so
 
 st.sidebar.header("Patient data")
 with st.sidebar:
-    age = st.slider(label="(Age)", min_value=30, max_value=90, step=1)
-    male = st.sidebar.radio("(Gender)", options=['Male', 'Female'], index=0)
-    anemic = st.checkbox("(Anemic)", 0)
-    cadiopathic = st.checkbox("(Cadiopathic)", 0)
-    diabetic = st.checkbox("(Diabetic)", 0)
-    Hypertension_episodes = st.checkbox("(Hypertension Episodes)", 0)
-    ast = st.slider(label="(AST)", min_value=6.0, max_value=1300.0, step=0.1)
-    chlorine = st.slider(label='(Chlorine)', min_value=83.0, max_value=120.0, step=0.1)
-    potassium = st.slider(label="(Pottasium)", min_value=2.00, max_value=8.00, step=0.01)
-    creatinine = st.slider(label='(Creatinine)', min_value=1.60, max_value=7.80, step=0.01)
-    creatinine_delta = st.slider(label='(Creatinine delta)', min_value=-5.00, max_value=8.00, step=0.01)
-    erythrocytes = st.slider(label="(Erythrocytes)", min_value=1.80, max_value=6.00, step=0.01)
-    erythrocytes_delta = st.slider(label="(Erythrocytes delta)", min_value=-2.00, max_value=1.30, step=0.01)
-    GFR_delta_last_4_months = st.slider(label="(GFR delta last 4 months)", min_value=-16.00,
+    age = st.slider(label="Age", min_value=30, max_value=90, step=1)
+    male = st.sidebar.radio("Gender", options=['Male', 'Female'], index=0)
+    anemic = st.checkbox("Anemic", 0)
+    cadiopathic = st.checkbox("Cadiopathic", 0)
+    diabetic = st.checkbox("Diabetic", 0)
+    Hypertension_episodes = st.checkbox("Hypertension Episodes", 0)
+    ast = st.slider(label="AST", min_value=6.0, max_value=1300.0, step=0.1)
+    chlorine = st.slider(label='Chlorine', min_value=83.0, max_value=120.0, step=0.1)
+    potassium = st.slider(label="Pottasium", min_value=2.00, max_value=8.00, step=0.01)
+    creatinine = st.slider(label='Creatinine', min_value=1.60, max_value=7.80, step=0.01)
+    creatinine_delta = st.slider(label='Creatinine delta', min_value=-5.00, max_value=8.00, step=0.01)
+    erythrocytes = st.slider(label="Erythrocytes", min_value=1.80, max_value=6.00, step=0.01)
+    erythrocytes_delta = st.slider(label="Erythrocytes delta", min_value=-2.00, max_value=1.30, step=0.01)
+    GFR_delta_last_4_months = st.slider(label="GFR delta last 4 months", min_value=-16.00,
                                         max_value=36.00, step=0.01)
-    GFR_delta_last_year = st.slider(label="(GFR delta last year)", min_value=-19.00,
+    GFR_delta_last_year = st.slider(label="GFR delta last year", min_value=-19.00,
                                     max_value=50.00, step=0.01)
-    GFR_standard_deviation_last_4_month = st.slider(label="(GFR standard deviation last 4 month)",
+    GFR_standard_deviation_last_4_month = st.slider(label="GFR standard deviation last 4 month",
                                                     min_value=0.00,
                                                     max_value=16.00, step=0.01)
-    GFR_standard_deviation_last_year = st.slider(label="(GFR standard deviation last year)", min_value=0.00,
+    GFR_standard_deviation_last_year = st.slider(label="GFR standard deviation last year", min_value=0.00,
                                                  max_value=16.00, step=0.01)
-    last_GFR = st.slider(label="(last GFR)", min_value=15.00,max_value=35.00)
-    hematocrit = st.slider(label="(Hematorit)", min_value=15.0, max_value=40.0, step=0.1)
-    hemoglobin = st.slider(label="(Hemoglobin)", min_value=0.1, max_value=16.0, step=0.1)
-    mean_corpuscular_hemoglobin = st.slider(label='(MCH)', min_value=20.0, max_value=39.0, step=0.1)
-    mean_corpuscular_volume = st.slider(label='(MCV)', min_value=67.0, max_value=112.0, step=0.1)
-    sodium = st.slider(label='(Sodium)', min_value=120, max_value=160, step=1)
-#     specific_gravity_standard_deviation = st.text_input(label='比重标准偏差(SGSTDV)',value='0.0000045')
-    specific_gravity_standard_deviation = st.number_input(label='(SGSTDV)',min_value=0.000000,step=0.000001,max_value=0.0005,value=0.0000045,format="%.6f")
-    urate = st.slider(label='(Urate)', min_value=3.00, max_value=9.00, step=0.01)
-    urea = st.slider(label='(Urea)', min_value=50.00, max_value=200.00, step=0.01)
-    urea_delta = st.slider(label='(Urea delta)', min_value=-60.00, max_value=60.00, step=0.01)
+    last_GFR = st.slider(label="last GFR", min_value=15.00,max_value=35.00)
+    hematocrit = st.slider(label="Hematorit", min_value=15.0, max_value=40.0, step=0.1)
+    hemoglobin = st.slider(label="Hemoglobin", min_value=0.1, max_value=16.0, step=0.1)
+    mean_corpuscular_hemoglobin = st.slider(label='MCH', min_value=20.0, max_value=39.0, step=0.1)
+    mean_corpuscular_volume = st.slider(label='MCV', min_value=67.0, max_value=112.0, step=0.1)
+    sodium = st.slider(label='Sodium', min_value=120, max_value=160, step=1)
+#     specific_gravity_standard_deviation = st.text_input(label='SGSTDV',value='0.0000045')
+    specific_gravity_standard_deviation = st.number_input(label='SGSTDV',min_value=0.000000,step=0.000001,max_value=0.0005,value=0.0000045,format="%.6f")
+    urate = st.slider(label='Urate', min_value=3.00, max_value=9.00, step=0.01)
+    urea = st.slider(label='Urea', min_value=50.00, max_value=200.00, step=0.01)
+    urea_delta = st.slider(label='Urea delta', min_value=-60.00, max_value=60.00, step=0.01)
     features = {"age": age,
                 "anemic": anemic,
                 "aspartate aminotransferase": ast,
